b3e2bbe81ffc25b4fb29e15802039bf9996f63fb
Lê Thị Thuỷ Tiên <1204thuytien@gmail.com>
2023-03-01 13:41:15 +0700

    update color

--- src/components/contains/color.js
+++ src/components/contains/color.js
@@ -2,8 +2,8 @@ export default {
  black: '#000000',
  white: '#FFFFFF',
  background: '#F1F1F1',
- text: '#090F24',
- color_hint: '#BEC0C7',
+ textTitle: '#090F24',
+ text_hint: '#BEC0C7',
  primaryColor: '#5CCAD3',
  secondColor: '#5ACBD1',
  color_button: '#51c282'
